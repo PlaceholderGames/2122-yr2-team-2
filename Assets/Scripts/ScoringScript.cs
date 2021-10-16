d1cf89f29a0cb8a125d0b61b0fe8a3fcb5bae4bc
Victor Almeida <30001606@students.southwales.ac.uk>
2021-10-16 15:15:47 +0100

    Added a win condition and tried adding enemies which failed

--- Assets/Scripts/ScoringScript.cs
+++ Assets/Scripts/ScoringScript.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoringScript : MonoBehaviour
 {
 
     public GameObject scoreText;
+    
 
     void OnTriggerEnter(Collider other)
     {
         GameVariables.theScore += 1;
         scoreText.GetComponent<Text>().text = "Pieces collected: " + GameVariables.theScore;
         Destroy(gameObject);
+
+        if(GameVariables.theScore == 4)
+        {
+            SceneManager.LoadScene("WinMenu", LoadSceneMode.Additive);
+        }
 
     }
 
