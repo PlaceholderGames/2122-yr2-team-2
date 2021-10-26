b6f0ee322dab1d859b66e0980e547e80926da19e
Victor Almeida <30001606@students.southwales.ac.uk>
2021-10-26 07:58:02 +0100

    Bug fix: The lose screen now shows up at or below 0 health, also made the cursor show in the lose screen.

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class GameOverScreen : MonoBehaviour
 {
 
+    public void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void RestartButton()
     {
         SceneManager.LoadScene("Level1");
